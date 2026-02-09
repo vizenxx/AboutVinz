829fc0762964a0c13f131fa32a6d682393f1b2e5
vizenxx <vizenx@gmail.com>
2026-02-09 13:35:27 +0600

    Configure custom domain vinz.info

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+<<<<<<< HEAD
 });
 
+=======
+  base: '/',
+  server: {
+    host: true,
+    port: 5173,
+  }
+})
+>>>>>>> 1d779f1 (Configure custom domain vinz.info)
